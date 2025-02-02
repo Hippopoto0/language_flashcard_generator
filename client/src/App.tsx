2f94a4a55845ae52870a214670d4f88b9c4aa68d
Hippopoto0 <danjones142004@gmail.com>
2025-02-02 16:15:50 +0000

    changed from localhost to docker0 address, hopefully works on aws

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,7 +15,8 @@ function App() {
 
   async function getFlashcards() {
     try {
-      const response = await fetch(`http://localhost:8080/${encodeURIComponent(currentLang)}`);
+      // const response = await fetch(`http://localhost:8080/${encodeURIComponent(currentLang)}`);
+      const response = await fetch(`http://172.17.0.1:8080/${encodeURIComponent(currentLang)}`);
       if (!response.ok) {
         throw new Error(`Error: ${response.statusText}`);
       }
